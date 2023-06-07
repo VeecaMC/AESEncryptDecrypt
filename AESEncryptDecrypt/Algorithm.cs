05b5d5a8b0ae665a741d50f4140ade7fb2bb9f62
petr.vecko <petr.vecko@homecredit.eu>
2023-06-07 09:35:46 +0200

    feat: Add solution and code

---
+++ AESEncryptDecrypt/Algorithm.cs
@@ -0,0 +1,73 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AESEncryptDecrypt;
+
+public class Algorithm
+{
+
+    public static async Task EncryptString(string sourceFilePath, string destinationFilePath, string strPwd)
+    {
+        try
+        {
+            await using (FileStream fileStream = new(destinationFilePath, FileMode.OpenOrCreate))
+            {
+                using Aes aes = Aes.Create();
+                byte[] key = Encoding.UTF8.GetBytes(strPwd);
+                aes.Key = key;
+
+                byte[] iv = aes.IV;
+                fileStream.Write(iv, 0, iv.Length);
+
+                await using CryptoStream cryptoStream = new(
+                           fileStream,
+                           aes.CreateEncryptor(),
+                           CryptoStreamMode.Write);
+                await using StreamWriter encryptWriter = new(cryptoStream);
+                await encryptWriter.WriteLineAsync(await File.ReadAllTextAsync(sourceFilePath, Encoding.UTF8));
+            }
+
+            Console.WriteLine("The file was encrypted.");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"The encryption failed.\n\n {exception}");
+        }
+    }
+
+    public static async Task DecryptString(string sourceFilePath, string destinationFilePath, string strPwd)
+    {
+        try
+        {
+            await using FileStream fileStream = new(sourceFilePath, FileMode.Open);
+            using Aes aes = Aes.Create();
+            byte[] iv = new byte[aes.IV.Length];
+            int numBytesToRead = aes.IV.Length;
+            int numBytesRead = 0;
+            while (numBytesToRead > 0)
+            {
+                int n = fileStream.Read(iv, numBytesRead, numBytesToRead);
+                if (n == 0) break;
+
+                numBytesRead += n;
+                numBytesToRead -= n;
+            }
+
+            byte[] key = Encoding.UTF8.GetBytes(strPwd);
+
+            await using CryptoStream cryptoStream = new(
+                       fileStream,
+                       aes.CreateDecryptor(key, iv),
+                       CryptoStreamMode.Read);
+            using StreamReader decryptReader = new(cryptoStream);
+            string decryptedMessage = await decryptReader.ReadToEndAsync();
+            Console.WriteLine($"The decrypted file text:\n\n {decryptedMessage}");
+            await File.WriteAllTextAsync(destinationFilePath, decryptedMessage, Encoding.UTF8);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"The decryption failed.\n\n {exception}");
+        }
+    }
+
+}
---
+++ AESEncryptDecrypt/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace AESEncryptDecrypt;
+
+public class Program
+{
+
+    public static async Task Main(string[] args)
+    {
+
+        while (true)
+        {
+            await ProcessEncryptDecrypt();
+        }
+    }
+
+
+    public static async Task ProcessEncryptDecrypt()
+    {
+        int iChoice = 0;
+        string strPwd = string.Empty;
+        string sourceFilePath = string.Empty;
+        string destinationFilePath = string.Empty;
+        
+        Console.WriteLine("Enter your choice:");
+        Console.WriteLine("1.Encryption   2.Decryption  3.Exit ");
+        Console.WriteLine("...............");
+        
+        iChoice = Convert.ToInt32(Console.ReadLine());
+
+        switch (iChoice)
+        {
+            case 1:
+                Console.WriteLine("Enter the filepath to a file containing the text to encrypt:");
+                sourceFilePath = Convert.ToString(Console.ReadLine()!);
+
+                Console.WriteLine("Enter the filepath to a target file:");
+                destinationFilePath = Convert.ToString(Console.ReadLine()!);
+
+                Console.WriteLine("Enter the Password:");
+                strPwd = Convert.ToString(Console.ReadLine()!);
+
+                await Algorithm.EncryptString(sourceFilePath, destinationFilePath, strPwd);
+                break;
+            case 2:
+                Console.WriteLine("Enter the filepath to a file containing the text to decrypt:");
+                sourceFilePath = Convert.ToString(Console.ReadLine()!);
+
+                Console.WriteLine("Enter the filepath to a target file:");
+                destinationFilePath = Convert.ToString(Console.ReadLine()!);
+
+                Console.WriteLine("Enter the Password:");
+                strPwd = Convert.ToString(Console.ReadLine()!);
+
+                await Algorithm.DecryptString(sourceFilePath, destinationFilePath, strPwd);
+                break;
+            default:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+}
